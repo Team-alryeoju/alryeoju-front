a70ba0b7c56ebe0f54f1235c2646a27f57135215
chyerin802 <chyerin802@gmail.com>
2022-10-10 21:27:37 +0900

    [FIX] Change slide to swiper/ Plus scroll to top

--- src/Components/ProductSlide.js
+++ src/Components/ProductSlide.js
@@ -7,52 +7,47 @@ import './ProductSlide.css'
 const ProductSlide = () => {
     // 카드 + margin 값 -> 220px
     // css 가 변경되면 값도 변경되어야 함!
+    // 카드는 총 15개가 들어갈 예정
     const cardSize = 220;
 
-    // product-slide__wrapper의 width를 가져오기 위해 useRef 사용한다.
     const ref = useRef()
 
-    // 페이지 당 들어갈 카드 수 limit (state)
-    const [limit, setLimit] = useState(3);
+    const [limit, setLimit] = useState(3); // 페이지 당 들어갈 카드 수 limit (state)
+    const [page, setPage] = useState(1); // 현재 페이지 번호 page (state)
 
-    // 현재 페이지 번호 page (state)
-    const [page, setPage] = useState(1);
-
-    // 현재 페이지의 첫번째 요소의 index 구하기
-    let offset = (page - 1) * limit; 
-
-    // 전체 페이지 계산 : 전체 데이터의 개수를 페이지 당 표시할 개수로 나눈다.
-    const numPage = Math.ceil(dummySool.length / limit)
+    // const offset = (page - 1) * limit; // 현재 페이지의 첫번째 요소의 index 구하기
+    const numPage = Math.ceil(dummySool.length / limit) // 전체 페이지 계산 : 전체 데이터의 개수를 페이지 당 표시할 개수로 나눈다.
+    // const showCard = 1;
 
     const changeWidth = () => {
         let newWidth = ref.current.clientWidth;
         
         setPage(1);
-        // if(newWidth > cardSize * 5) setLimit(5);
-        // else if(newWidth > cardSize * 4) setLimit(4);
-        if(newWidth > cardSize * 3) setLimit(3);
-        else if(newWidth > cardSize * 2) setLimit(2);
-        else setLimit(1);
+
+        if(newWidth <= cardSize + 1) setLimit(1);
+        else if(newWidth <= cardSize * 2 + 1) setLimit(2);
+        else setLimit(3);
     }
+
+    // const handlePrevBtn = () => {
+    // }
 
     useEffect(() => {
         changeWidth();
-    }, [])
-
-    useEffect(() => {
         window.addEventListener("resize", changeWidth)
     }, [])
 
     return (
-        <div className="product-slide">
-            {/* 이전 페이지 이동 */}
-            <button onClick={() => setPage(page - 1)} disabled={page === 1}>left</button>
-            <ul className="product-slide__wrapper" ref={ref}>
-                {/* 페이지 별로 slice 해서 보여준다 */}
-                {dummySool.slice(offset, offset+limit).map((sool) => <li key={sool.id}><ProductCard sool={sool}/></li>)}
-            </ul>
-            {/* 다음 페이지 이동 */}
-            <button onClick={() => setPage(page + 1)} disabled={page === numPage}>right</button>
+        <div className="product-slide col-center">
+            <div className="product-slide__main row-center">
+                <div className="product-slide__wrapper row">
+                    <button onClick={() => setPage(page - 1)} disabled={page === 1}>prev</button>
+                    <ul className="product-slide__content row" ref={ref}>
+                        {dummySool.map((sool) => <li key={sool.id}><ProductCard sool={sool}/></li>)}
+                    </ul>
+                    <button onClick={() => setPage(page + 1)} disabled={page === numPage}>next</button>
+                </div>
+            </div>
         </div>
     )
 }
