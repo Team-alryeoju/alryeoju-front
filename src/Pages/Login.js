3b4c9448e78cd95a461e84f8ac15168508e74b89
chyerin802 <chyerin802@gmail.com>
2022-10-10 23:59:43 +0900

    git ignore

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,17 +4,14 @@ function Login(props) {
     return (
         <div className='Login'>
             <form id="login" action="/login" method="post" className="input-group">
-                <div>
-                    <input type="text" name="id" className="input-field" placeholder="아이디" required></input>
-                    <button>중복확인</button>
-                </div>
+                <input type="text" name="id" className="input-field" placeholder="아이디" required></input>
                 <input type="password" name="pw" className="input-field" placeholder="비밀번호" required></input>
-                <input type="password" name="pw" className="input-field" placeholder="비밀번호 확인" required></input>
                 <button className="submit">Login</button>
             </form>
             
         </div>
     );
 }
+
 
 export default Login;
