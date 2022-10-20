624ac82e74d776ae55dd8a9861fa949cb7986096
chyerin802 <chyerin802@gmail.com>
2022-10-20 23:16:22 +0900

    [FEAT] 홈페이지 - 상품 리스트 카테고리 추가

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 import './ProductCard.css'
 
 const ProductCard = ({ sool }) => {  
-    const url = `/detail/${sool.id}`
+    const url = `/detail/${sool.al_id}`
     const textDecoNone = {
       textDecoration: 'none'
     };
 
     return(
       <Link className="productCard row-center" to={url} style={textDecoNone}>
-        <div className="productCard__wrapper" id={sool.id}>
+        <div className="productCard__wrapper" id={sool.al_id}>
               <div className="productCard--img">
-                <img src={sool.img} alt={sool.name}></img>  
+                <img src={sool.img_link} alt={sool.al_name}></img>  
               </div>
               <div className="productCard__content col-center">
-                <span className="productCard--name">{sool.name}</span>
+                <span className="productCard--name">{sool.al_name}</span>
                 <span className="productCard--price">1000원</span>
               </div>
         </div>
