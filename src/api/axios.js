0e4c97a2071ccd8c2609cfb3f187cceceba070fa
chyerin802 <chyerin802@gmail.com>
2022-11-19 14:47:31 +0900

    Deploy Test with Server

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 모든 요청에 withCredentials가 true로 설정됩니다.
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://127.0.0.1:5000';
+axios.defaults.baseURL = 'http://54.180.81.77:5000';
 export default axios
 
 // export default axios.create({
