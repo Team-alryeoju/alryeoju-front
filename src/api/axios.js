dfa9987417a6e2c1588555d66a10bdec1392ede8
chyerin802 <chyerin802@gmail.com>
2022-11-23 22:12:57 +0900

    배포 테스트

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 // 모든 요청에 withCredentials가 true로 설정됩니다.
+// 배포용!
 axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'https://alryeoju.netlify.app/api';
+export default axios
 
-export default axios.create({
-    baseURL: 'http://127.0.0.1:5000'
-})
+// export default axios.create({
+//     baseURL: 'http://127.0.0.1:5000'
+// })
