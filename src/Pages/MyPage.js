8af9bdee7aeb3952cdae7187db484d1d6da5d198
chyerin802 <chyerin802@gmail.com>
2022-11-24 10:30:41 +0900

    구매내역 아래 여백 수정

--- src/Pages/MyPage.js
+++ src/Pages/MyPage.js
@@ -12,6 +12,7 @@ import styled from "styled-components";
 
 const MyPageContainer = styled.div`
     margin-top: 40px;
+    margin-bottom: 40px;
     display: flex;
     flex-direction: column;
     align-items: center;
